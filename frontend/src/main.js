dbddf20f610542bbaa47988feebc6eb15bce71ea
Michał Hubert <michhub402@student.polsl.pl>
2022-04-11 20:38:51 +0200

    Fix modules versions, install dependencies, configure application

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+const backendApi = axios.create({
+  baseURL: "http://127.0.0.1:5000/api"
+});
+
+Vue.prototype.backendApi = backendApi;
+
+Vue.use(require("vue-cookies"));
 
 new Vue({
   vuetify,
+  router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
