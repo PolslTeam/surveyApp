5cbfbe41c7ac67543c3ff7da5b63e8a3f47a00b6
Adrian Pietrowski <adrian.pietrowski@fss.cc>
2022-05-09 16:34:48 +0200

    #TAB-12 Backend: Added editForm endpoint

--- backend/routes/editFormRoute.js
+++ backend/routes/editFormRoute.js
@@ -53,4 +53,156 @@ router.get("/getForm", async (req, res) =>{
   }
 });
 
+router.put("/editForm", async(req,res) => {
+  const transaction = await db.transaction();
+
+  const res400 = async (form_pos, message) => {
+    await transaction.rollback();
+    return res.status(400).json({form_pos, message});
+  }
+
+  try {
+    const {settings, fields, user_id} = req.body;
+
+    const form = await Form.findOne({
+      where: {
+        form_id,
+        user_id
+      },
+      include: [
+        {
+          model: Text_field,
+          as: 'Text_fields'
+        },
+        {
+          model: Slider_field,
+          as: 'Slider_fields'
+        },
+        {
+          model: Singlechoice_field,
+          as: 'Singlechoice_fields'
+        }
+      ]
+    });
+
+    if(!settings.title)
+      return res.status(400).json({message: 'Missing title'});
+
+    if(!fields.length)
+      return res.status(400).json({message: 'Add at least 1 field'});
+
+    if(!settings.authenticationType)
+      return res.status(400).json({message: 'Missing authentication type'});
+    settings.login_required = settings.authenticationType === 'logged';
+
+    for(const [form_pos, field] of Object.entries(fields)) {
+      if(!field.question.length)
+        return res400(form_pos, 'Empty question.');
+
+      const props = {
+        form_id: form.form_id,
+        form_pos,
+        ...field
+      };
+
+      switch(field.type) {
+        case 'text':
+          if(field.minLength < 0)
+            return res400(form_pos, 'Minimal length must be greater than 0');
+
+          if (!field.text_field_id) {
+            await Text_field.create(props, {transaction})
+          } else {
+            const text = await Text_field.findOne({where: {text_field_id: field.text_field_id}})
+            await text.update({
+              question: field.question,
+              min_length: field.min_length,
+              form_pos,
+              required: field.required
+            })
+            await text.save();
+          }
+          continue;
+        case 'slider':
+          if(isNaN(field.min) || isNaN(field.max) || field.min < 0 || field.max < 0 || field.max - field.min < 1)
+            return res400(form_pos, 'Provide correct min/max values');
+          if (!field.text_field_id) {
+            await Slider_field.create(props, {transaction});
+          } else {
+            const slider = await Slider_field.findOne({where: {slider_field_id: field.slider_field_id}})
+            await slider.update({
+              question: field.question,
+              min: field.min,
+              max: field.max,
+              form_pos,
+              required: field.required
+            })
+            await slider.save();
+          }
+          continue;
+
+        case 'list':
+        case 'single choice':
+          if(field.choices.length < 2)
+            return res400(form_pos, 'Provide at least 2 answers');
+          if(field.choices.some(ans => !ans))
+            return res400(form_pos, 'Some answers are empty');
+          if (!field.singlechoice_field_id) {
+            const {singlechoice_field_id} = await Singlechoice_field.create({
+              is_list: field.type === 'list',
+              ...props
+            }, {transaction});
+            for (const [option_pos, option] of Object.entries(field.choices))
+              await Choice_option.create({
+                singlechoice_field_id,
+                option_pos,
+                option
+              }, {transaction});
+          } else {
+            const singlechoice = await Singlechoice_field.findOne({
+              singlechoice_field_id: field.singlechoice_field_id
+            });
+            await singlechoice.update({
+              is_list: field.type === 'list',
+              question: field.question,
+              form_pos: form_pos,
+              required: field.required,
+            })
+            await singlechoice.save()
+            for (const option of Object.entries(field.choices))
+              if (!option.option_id) {
+                await Choice_option.create({
+                  singlechoice_field_id: field.singlechoice_field_id,
+                  option_pos: option.option_pos,
+                  option: option.option
+                }, {transaction});
+              } else {
+                const foundOption = await Choice_options.findOne({
+                  where:{
+                    singlechoice_field_id: field.singlechoice_field_id,
+                    option_id: field.option_id
+                  }
+                })
+                await foundOption.update({
+                  option: option.option,
+                  option_pos: option.option_pos
+                })
+                await foundOption.save()
+              }
+          }
+          continue;
+
+        default:
+          return res400(form_pos, `Invalid field type "${field.type}"`);
+      }
+    }
+    await transaction.commit();
+    res.json(form);
+  } catch (e) {
+    await transaction.rollback();
+    console.log(e.message);
+    res.status(500).json({error: e.message});
+  }
+})
+
 module.exports = router;
